833a96fa3dd421105acfce4dc81a4376595359f0
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2024-10-25 10:36:19 +0530

    Intializing the spring-boot project

---
+++ src/main/java/dev/raveen/runnerz/WelcomeMessage.java
@@ -0,0 +1,14 @@
+package dev.raveen.runnerz;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeMessage {
+
+    public String getWelcomeMessage() {
+        return "Welcome to the Spring Boot Application.";
+    }
+
+}
+
+
